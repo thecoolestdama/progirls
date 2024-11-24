1292dd4b17f4535ca72c9d9abdfaaa12defa42cd
categoricalcat <wallacefares@gmail.com>
2024-11-24 14:51:02 -0300

    test: console warn

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const basePath = process.env.BASE_PATH ?? undefined;
+if (!basePath) console.warn("BASE_PATH env is not defined");
 
 const nextConfig: NextConfig = {
   output: "export",
