6209f086c297632137796e1bf53d13d2ee1c12c1
Dâmarys Santos <damarysdev@gmail.com>
2024-12-30 17:28:16 -0300

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,15 +14,21 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ name, state, imageUrl, description, linkedinUrl, githubUrl, externalUrl }) => {
   return (
     <div className="card">
+      <div className="card-gradient"></div>
+
       <div className="card-header">
-        <span>{name}</span> | <span>{state}</span>
+        <h2 className="card-name">{name}</h2>
+        <p className="card-location">{state}</p>
       </div>
+
       <div className="card-image-container">
         <img src={imageUrl} alt={name} className="card-image" />
       </div>
+
       <div className="card-description">
         <p>{description}</p>
       </div>
+
       <div className="card-icons">
         <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
           <img src="/icons/linkedin.png" alt="LinkedIn" />
@@ -34,7 +40,6 @@ const Card: React.FC<CardProps> = ({ name, state, imageUrl, description, linkedi
           <img src="/icons/external-link.png" alt="External Link" />
         </a>
       </div>
-      <div className="card-circle"></div>
     </div>
   );
 };
